ebfd71ac3bb1b1156bb385569ee9b588fce2a060
AnastasyMeleshko <7634907@mail.ru>
2022-03-16 18:41:24 +0300

    add shadow block and main popap

--- js/index.js
+++ js/index.js
@@ -3,10 +3,24 @@ import { showTimer } from "./showTimer.js";
 document.addEventListener('DOMContentLoaded', showTimer);
 document.addEventListener('DOMContentLoaded', formValidation);
 
+// function insertGifForSearch() {
+//     let search = document.createElement("div");
+//     search.classList.add("search-active");
+//     return search;
+// }
+
 function formValidation() {
     const form  = document.querySelector(".footer-form");
     const formWrap = document.querySelector(".form-wrapper");
     const input = document.querySelector("#mail");
+    const modal = document.getElementById("modal");
+    // let modalForSearch =  modal.cloneNode(true);
+    // modalForSearch.innerHTML = "";
+    // modalForSearch.classList.add("search-active");
+    // console.log(modalForSearch)
+    let informMessage;
+
+    // let newSearch = insertGifForSearch();
 
     form.addEventListener("submit", formSend);
 
@@ -20,43 +34,75 @@ function formValidation() {
         if (error === 0) {
             formRemoveError(input);
             input.classList.add("good");
-
-            // let requestEmail = input.value.match(/[\s\S]*?(?=@)/)[0];
-
             formWrap.classList.add("sending");
             let response = await fetch(`https://reqres.in/api/login`, {
                 method : "POST",
                 body : formData
             });
-
+            // document.querySelector(".container").append(modalForSearch);
             if (response.ok) {
+
+                // modal.innerHTML = "";
+                // modal.append(newSearch);
                 let result = await response.json();
-                alert(result.message);
+                setTimeout(() => {
+                    // modalForSearch.style.opacity = "1";
+                        alert(result.message);
+                        informMessage = "result.message";
+                    }, 3000);
                 form.reset();
-                formWrap.classList.remove("sending");
+                setTimeout(() => { formWrap.classList.remove("sending"); }, 3000);
                 input.classList.remove("good");
             } else {
-                formWrap.classList.remove("sending");
-                input.classList.remove("good");
-                alert("Ошибка");
-            }
+                // modal.innerHTML = "";
+                // modal.append(newSearch);
+                // modalForSearch.style.opacity = "1";
+                setTimeout( () => {
+                    formWrap.classList.remove("sending");
+                    input.classList.remove("good");
+                }, 2500);
+                setTimeout(() => {
+                    response.json().then(error => {
+                        const e = new Error('Ooops...Something went wrong')
+                        e.data = error
+                        throw error
+                    })
+                    // modal.innerHTML = "";
+                    // modal.append(newSearch);
+                    alert("Ooops...Something went wrong");
+                    informMessage = "Ooops...Something went wrong";
+                    }, 3000);
 
-        } else {
-            alert("Зполните поля")
+        }} else {
+            setTimeout(() => {
+                alert("Please enter an e-mail address!");
+                informMessage = "Please enter your e-mail address!";
+                }, 300);
+            formAddError(input);
         }
 
         function formValidate(form) {
             let error = 0;
-            let formReq = document.querySelector('.req');
 
             formRemoveError(input);
 
-            if (emailTest(input)) {
+            if (!emailTest(input)) {
                 formAddError(input);
                 error++;
             } else if (input.value === "") {
                 formAddError(input);
                 error++;
+            } else if (input.value.length < input.minLength) {
+                formAddError(input);
+                error++;
+            }
+
+            function emailTest(input) {
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value))
+                {
+                    return (true);
+                }
+                return (false);
             }
             return error;
         }
@@ -70,9 +116,10 @@ function formValidation() {
             input.classList.remove("error");
             input.parentElement.classList.remove("error");
         }
-
-        function emailTest(input) {
-            return !/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(input.value);
-        }
     }
 }
+
+let currentUrl = window.location.href;
+let logoLink = document.querySelector(".logo-link");
+logoLink.setAttribute("href", `${currentUrl}`);
+
